588b5b3a4ebe174a208ff56ea37fe51481fb59e6
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-01-29 21:49:57 +0530

    aws CLI

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -134,6 +134,7 @@ AWS_STORAGE_BUCKET_NAME = 'sumannayika'
 AWS_S3_REGION_NAME = 'ap-south-1'  # e.g., 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'  # Allow public access
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
