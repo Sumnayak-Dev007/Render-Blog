e2e51757d3818768140bbd4cbc7cc9f1113d92a7
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-01-29 09:02:50 +0530

    collectstatic for admin dashboard

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -128,10 +128,10 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = BASE_DIR / 'assets'
-MEDIA_ROOT = BASE_DIR/'media'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/uploads/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
