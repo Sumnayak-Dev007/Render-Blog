15c7ca6bf7485169fec34adac9eed807187e0a5d
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-01-29 11:24:54 +0530

    media files

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -24,9 +24,8 @@ urlpatterns = [
     path('', include('myBlog.urls')),
     path('', include('user.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
 
 
 
