99200f346f1786d9f2018e1a74c544b67c6e47cf
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-01-29 11:36:49 +0530

    mediasss

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -15,16 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static,serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myBlog.urls')),
     path('', include('user.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    # Serve media files in development using Django's static function
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files in production using 'serve' view
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
     
 
 
