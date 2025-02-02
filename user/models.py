bbff4a7cf1173b0bd5d3810ad8ea2c929143462d
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-02-02 10:19:25 +0530

    fixing time zone

--- user/models.py
+++ user/models.py
@@ -23,3 +23,44 @@ class Profile(models.Model):
         return self.user.username
     
 
+# [Unit]
+# Description=gunicorn service
+# After=network.target
+
+# [Service]
+# User=ubuntu
+# Group=www-data
+# WorkingDirectory=/home/ubuntu/Project/Render-Blog
+# ExecStart=/home/ubuntu/Project/venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:/home/ubuntu/Project/Render-Blog/DjangoBlog.sock DjangoBlog.wsgi:application
+# Environment="my_email=nsumankumari225@gmail.com"
+# Environment="epass=byqu kyge gbqb ygzi"
+# [Install]
+# WantedBy=multi-user.target
+
+
+# server {
+#        listen 80;    
+#        server_name 13.233.133.43;
+#        location = /favicon.ico {access_log off;log_not_found off;} 
+    
+    
+#         location / {
+#             include proxy_params;
+#             proxy_pass http://unix:/home/ubuntu/Project/Render-Blog/DjangoBlog.sock
+#         }
+#      }
+
+# server {
+#     listen 80;
+#     server_name 13.233.133.43;
+    
+#     location = /favicon.ico {
+#         access_log off;
+#         log_not_found off;
+#     }
+
+#     location / {
+#         include proxy_params;
+#         proxy_pass http://unix:/home/ubuntu/Project/Render-Blog/DjangoBlog.sock;
+#     }
+# }
